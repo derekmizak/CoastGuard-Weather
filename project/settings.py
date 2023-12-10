3c831a80262b4539a8c1f256623522d47aa6ade9
Derek Mizak <dm@emizak.com>
2023-12-10 23:47:22 +0000

    fixing csrf

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.getenv('SITE_NAME').split(',')
+CORS_ORIGIN_WHITELIST = os.getenv('SITE_NAME').split(',')
 # Application definition
 
 INSTALLED_APPS = [
