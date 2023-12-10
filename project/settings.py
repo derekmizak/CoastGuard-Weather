5b6d44a260f826d45b8434ff252be99c2df3915d
Derek Mizak <dm@emizak.com>
2023-12-10 16:40:30 +0000

    ssl issue troubleshooting

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = STATIC_URL
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
