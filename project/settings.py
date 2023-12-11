50afc2502e0519a807345d9563346bf76db9de0f
Derek Mizak <dm@emizak.com>
2023-12-11 00:03:14 +0000

    fixing csrf

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS ').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://cg-weather.redblue.ai']
 CORS_ORIGIN_WHITELIST = os.getenv('SITE_NAME').split(',')
 CSRF_COOKIE_SECURE = True
 # Application definition
